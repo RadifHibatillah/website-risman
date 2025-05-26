8073c7d0ae3e29c2fc992cdb502235a0a638beda
Muhammad Radifah Hibatillah <radifhibatillah8@gmail.com>
2025-05-26 16:27:58 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+ <script>
+  const toggle = document.getElementById('nav-toggle');
+  const menu = document.getElementById('nav-menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+</script>
