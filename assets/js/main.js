3849232c5401135e8659578bb44728782f049e39
Muhammad Radifah Hibatillah <radifhibatillah8@gmail.com>
2025-06-01 17:16:17 +0700

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
- <script>
-  const toggle = document.getElementById('nav-toggle');
-  const menu = document.getElementById('nav-menu');
-
-  toggle.addEventListener('click', () => {
-    menu.classList.toggle('show');
-  });
-</script>
